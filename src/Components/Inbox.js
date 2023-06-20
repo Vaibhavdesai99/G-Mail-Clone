603da0306333a8d16a5d8ca5ed82eaf3b774fd97
Vaibhav Ramchandra Desai <127377578+Vaibhavdesai99@users.noreply.github.com>
2023-06-21 00:14:39 +0530

    Delete Mail

--- src/Components/Inbox.js
+++ src/Components/Inbox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { InboxActions } from './ReduxStore/InboxReducer'
 import './Inbox.css'
@@ -7,8 +7,11 @@ import { useDispatch,useSelector } from 'react-redux'
 const Inbox = () => {
     const dispatch = useDispatch()
     const InboxData = useSelector(state => state.inboxReducer.inboxData)
+    const updateCounterOnDelete = useSelector(state => state.inboxReducer.unread)
 
-    let url =  'https://emailbox-42ee5-default-rtdb.firebaseio.com'
+
+    // Delete Email :-
+    let url =   "https://mail-box-client-72ab7-default-rtdb.firebaseio.com/"
 
     const email = localStorage.getItem('email').replace(/['@','.']/g,'');
 
@@ -18,16 +21,26 @@ const Inbox = () => {
                const response = await fetch(`${url}/Inbox/${email}/${id}.json`,{
                 method:'DELETE'
                })
-               dispatch(InboxActions.updateGet())
+               console.log(response)
+               dispatch(InboxActions.changeInbox(InboxData.filter(item => item.id !=id)))
+               dispatch(InboxActions.updateUnread())
         } catch (error) {
             console.log(error)
             
         }
 }
 
+// Passing id -  to delete specific email :- To deleteData :-
 const deleteHandler =(id)=>{
-    deleteData(id)
+  deleteData(id)
 }
+
+// To delete when user click on delete , without refresh email get deleted ...fr that we use useEffect()
+useEffect(() => {
+  dispatch(InboxActions.updateGet());
+}, [dispatch]);
+
+
 
   return (
     <div className='ParentBox'>
@@ -49,12 +62,17 @@ const deleteHandler =(id)=>{
                         {InboxData.map((item,index)=>{
                             return(
                                 <tr key={item.id}>
+                                  {/* index+1 caz it starting from zero so index + 1 = 1 */}
                                  <td scope='row'>{index + 1}</td>
 
                                  {/* to show UNREAD MESSAGES  */}
                                  <td>{!item.read && <div style={{width:'10px',height:'10px',borderRadius:'100%',backgroundColor:'red'}}></div> }{item.from}</td>
                                  <td>{item.subject}</td>
+
+                                 {/* To View Specific Email  */}
                                  <td><Link to={`/Inbox/${item.id}`}>Open Message </Link></td>
+
+                                 {/* Passing id to delete sepcific Email  */}
                                  <td><button type='button' class='btn btn-danger' onClick={deleteHandler.bind(null,item.id)}>delete</button></td>
                                 </tr>
                             )
