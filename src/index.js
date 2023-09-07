1e51cfceb4a51c2f18bbab73286dedd90d0e501d
Vaibhav Ramchandra Desai <127377578+Vaibhavdesai99@users.noreply.github.com>
2023-09-07 20:10:11 +0530

    Added Comment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import store from "./Components/ReduxStore/Index";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
+// Bootstrap Added React-Bootstrap :
 import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
